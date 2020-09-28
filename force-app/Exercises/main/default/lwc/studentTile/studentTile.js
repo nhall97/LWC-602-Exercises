90710ecf546d73a19a79dcfa7346789dc57bcc17
Nathan <62209008+nhall97@users.noreply.github.com>
2020-09-28 14:30:00 +0100

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,14 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTile extends LightningElement {
+    @api selected = false;
+
+    @api student = {
+        Name: 'Nathan',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+    };
+
+    get tileSelected() {
+        return this.selected ? "tile selected " : "tile";
+        }
+}
