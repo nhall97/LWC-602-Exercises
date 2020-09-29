2f4ea2e82bfab4eba5ebb4b05492a9217721aa04
Nathan <62209008+nhall97@users.noreply.github.com>
2020-09-29 16:10:43 +0100

    ex 2-10 all files

---
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -0,0 +1,57 @@
+import { LightningElement, track, wire } from 'lwc';
+import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
+import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
+import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
+
+import FIELD_Name from '@salesforce/schema/Contact.Name';
+import FIELD_Description from '@salesforce/schema/Contact.Description';
+import FIELD_Email from '@salesforce/schema/Contact.Email';
+import FIELD_Phone from '@salesforce/schema/Contact.Phone';
+
+const fields = [FIELD_Name, FIELD_Description, FIELD_Email, FIELD_Phone];
+
+export default class StudentDetail extends LightningElement {
+
+	studentId;
+	@wire(MessageContext) messageContext;
+
+	@wire(getRecord, { recordId: '$studentId', fields })
+	wiredStudent;
+	subscription;
+	
+	connectedCallback() {
+		if(this.subscription){
+			return;
+		}
+		this.subscription = subscribe(this.messageContext, SELECTED_STUDENT_CHANNEL, 
+			message => {
+				this.handleStudentChange(message);
+			});
+	}
+
+	disconnectedCallback(){
+		unsubscribe(this.subscription);
+		this.subscription = null;
+	}
+
+	handleStudentChange(event) {
+		this.studentId = event.studentId;
+	}
+	
+	get name() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Name);
+	}
+	get description() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Description);
+	}
+	get phone() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Phone);
+	}
+	get email() {
+		return this._getDisplayValue(this.wiredStudent.data, FIELD_Email);
+	}
+	_getDisplayValue(data, field) {
+		return getFieldDisplayValue(data, field) ? getFieldDisplayValue(data, field) : getFieldValue(data, field);
+	}
+	
+}
--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,21 +1,22 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api} from 'lwc';
 
 export default class StudentTile extends LightningElement {
-    @api selectedStudentId = '';
+	@api student = {
+		Name: 'Nathan',
+		PhotoUrl: '/services/images/photo/003B0FakePictId',
+	};
+	@api selectedStudentId = '';
 
-    @api student = {
-        Name: 'Nathan',
-        PhotoUrl: '/services/images/photo/003B0FakePictId',
-    };
-
-    get tileSelected() {
-        return (this.selectedStudentId===this.student.Id) ? "tile selected" : "tile";
-    }
-
-    studentClick() {
-        const evt = new CustomEvent('studentselected', {
-            detail: { studentId: this.student.Id }
-        });
-        this.dispatchEvent(evt);
-    }
+	get tileSelected() {
+		return (this.selectedStudentId===this.student.Id) ? "tile selected " : "tile";
+	}
+	
+	studentClick(){
+		const evt = new CustomEvent('studentselected', {
+			bubbles: true, composed: true,
+			detail: { studentId: this.student.Id }
+		});
+		this.dispatchEvent(evt);
+	}
+	
 }
